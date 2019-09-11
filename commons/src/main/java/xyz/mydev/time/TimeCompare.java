92fa6fb44adc75952a38a7bc434b28de1d37ea5e
zhaoshipan <zhaoshipan@sishuxuefu.com>
2019-09-11 17:31:39 +0800

    'save'

---
+++ commons/src/main/java/xyz/mydev/time/TimeCompare.java
@@ -0,0 +1,21 @@
+package xyz.mydev.time;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+/**
+ * @author ZSP  2019/09/10 16:31
+ */
+public class TimeCompare {
+  public static void main(String[] args) {
+    LocalDateTime min = LocalDateTime.of(LocalDate.now(), LocalTime.MIN);
+    LocalDateTime min2 = LocalDateTime.of(LocalDate.now(), LocalTime.MIN);
+    System.out.println(min.isAfter(min2));
+    System.out.println(min.isBefore(min2));
+
+    System.out.println(LocalDateTime.now().toString());
+
+  }
+
+}
