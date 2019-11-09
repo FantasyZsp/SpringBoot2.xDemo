91fa503c841655d73b0c5bfe2ce635eac4427183
zhaoshipan <541054738@qq.com>
2019-11-09 17:57:11 +0800

    config

--- proxy-demo/src/main/java/xyz/mydev/jdk/Client.java
+++ proxy-demo/src/main/java/xyz/mydev/jdk/Client.java
@@ -14,6 +14,7 @@ import java.lang.reflect.Proxy;
 @Slf4j
 public class Client {
   public static void main(String[] args) throws Exception {
+    System.getProperties().put("sun.misc.ProxyGenerator.saveGeneratedFiles", "true");
     testByInnerClass();
   }
 
