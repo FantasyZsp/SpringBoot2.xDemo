048dc943ff30eb52d7f47c9be41987c95cd04ca0
ZSP-LAPTOP <541054738@qq.com>
2019-12-01 22:24:01 +0800

    nio

---
+++ jdk/src/main/java/xyz/mydev/jdk/nio/BufferTest.java
@@ -0,0 +1,61 @@
+package xyz.mydev.jdk.nio;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+/**
+ * @author ZSP
+ */
+public class BufferTest {
+  private final String modulePath = System.getProperty("user.dir");
+
+  public static void main(String[] args) throws Exception {
+    System.out.println(System.getProperty("user.dir"));
+    System.out.println(System.getProperty("user.home"));
+  }
+
+  @Before
+  public void before() {
+    System.out.println(modulePath);
+    System.out.println(System.getProperty("user.home"));
+  }
+
+
+  @Test
+  public void testChannelRead() throws Exception {
+    FileInputStream fileInputStream = new FileInputStream(modulePath + "/src/main/resources/nio-demo.txt");
+    FileChannel channel = fileInputStream.getChannel();
+    ByteBuffer byteBuffer = ByteBuffer.allocate(512);
+    channel.read(byteBuffer);
+    byteBuffer.flip();
+
+    while (byteBuffer.hasRemaining()) {
+      System.out.println((char) byteBuffer.get());
+    }
+  }
+
+  @Test
+  public void testChannelWrite() throws Exception {
+    String filePath = modulePath + "/src/main/resources/nio-write-test.txt";
+
+    FileOutputStream fileOutputStream = new FileOutputStream(filePath);
+    FileChannel channel = fileOutputStream.getChannel();
+    ByteBuffer byteBuffer = ByteBuffer.allocate(512);
+
+    byte[] content = "test write".getBytes();
+    for (byte b : content) {
+      byteBuffer.put(b);
+    }
+    byteBuffer.flip();
+    channel.write(byteBuffer);
+
+    System.out.println(FileUtils.readFileToString(new File(filePath)));
+  }
+}
