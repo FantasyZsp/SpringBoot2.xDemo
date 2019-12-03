6246568e372656e6bde1bb06bfe30988d3c74774
zhaoshipan <541054738@qq.com>
2019-12-03 18:43:46 +0800

    fix<nio>: 客户端断开连接后服务端仍读取

--- jdk/src/main/java/xyz/mydev/jdk/nio/ServerSocketChannelTest.java
+++ jdk/src/main/java/xyz/mydev/jdk/nio/ServerSocketChannelTest.java
@@ -2,10 +2,12 @@ package xyz.mydev.jdk.nio;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
+import java.util.Iterator;
 import java.util.Set;
 
 /**
@@ -25,31 +27,57 @@ public class ServerSocketChannelTest {
       InetSocketAddress inetSocketAddress = new InetSocketAddress(port);
       serverSocketChannel.bind(inetSocketAddress);
       serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+      System.out.println("监听端口：" + port);
     }
 
     while (true) {
       int select = selector.select();
+      System.out.println(select);
       Set<SelectionKey> selectionKeys = selector.selectedKeys();
+      System.out.println("selectionKeys: " + selectionKeys);
 
+      Iterator<SelectionKey> iterator = selectionKeys.iterator();
 
-      for (SelectionKey selectionKey : selectionKeys) {
-        ServerSocketChannel channel = (ServerSocketChannel) selectionKey.channel();
-        channel.configureBlocking(false);
-        SocketChannel socketChannel = channel.accept();
+      while (iterator.hasNext()) {
+        SelectionKey selectionKey = iterator.next();
         if (selectionKey.isAcceptable()) {
-          System.out.println("========isAcceptable========");
-          System.out.println(socketChannel);
-        } else if (selectionKey.isConnectable()) {
-          System.out.println("========isConnectable=========");
+          ServerSocketChannel serverSocketChannel = (ServerSocketChannel) selectionKey.channel();
+          SocketChannel socketChannel = serverSocketChannel.accept();
+          socketChannel.configureBlocking(false);
+          socketChannel.register(selector, SelectionKey.OP_READ);
 
+          System.out.println("获得客户端连接: " + socketChannel);
         } else if (selectionKey.isReadable()) {
-          System.out.println("========isReadable=========");
-        } else if (selectionKey.isValid()) {
-          System.out.println("========isValid=========");
-        } else if (selectionKey.isWritable()) {
-          System.out.println("========isWritable=========");
+          SocketChannel socketChannel = (SocketChannel) selectionKey.channel();
+          int byteReads = receiveAndReturnMsg(socketChannel);
         }
+        iterator.remove();
       }
+
+      selectionKeys.clear();
+
     }
+  }
+
+  private static int receiveAndReturnMsg(SocketChannel socketChannel) throws IOException {
+    int byteReads = 0;
+    while (true) {
+      ByteBuffer byteBuffer = ByteBuffer.allocate(512);
+      byteBuffer.clear();
+      int read = socketChannel.read(byteBuffer);
+      byteReads += read;
+      if (read == -1) {
+        socketChannel.close();
+        break;
+      }
+      if (read == 0) {
+        break;
+      }
+      byteBuffer.flip();
+      socketChannel.write(byteBuffer);
+    }
+    System.out.println("收到客户端[" + socketChannel.socket().getLocalPort() + "]消息: ");
+    System.out.println("读取" + byteReads + "个字节");
+    return byteReads;
   }
 }
