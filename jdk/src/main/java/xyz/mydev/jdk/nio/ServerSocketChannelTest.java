4f403d63bc417bf861e194de1a229ca74b5b1b6c
zhaoshipan <541054738@qq.com>
2019-12-03 17:49:23 +0800

    save

--- jdk/src/main/java/xyz/mydev/jdk/nio/ServerSocketChannelTest.java
+++ jdk/src/main/java/xyz/mydev/jdk/nio/ServerSocketChannelTest.java
@@ -5,6 +5,8 @@ import java.net.InetSocketAddress;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Set;
 
 /**
  * 一个选择器拥有多个通道
@@ -23,6 +25,31 @@ public class ServerSocketChannelTest {
       InetSocketAddress inetSocketAddress = new InetSocketAddress(port);
       serverSocketChannel.bind(inetSocketAddress);
       serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+    }
+
+    while (true) {
+      int select = selector.select();
+      Set<SelectionKey> selectionKeys = selector.selectedKeys();
+
+
+      for (SelectionKey selectionKey : selectionKeys) {
+        ServerSocketChannel channel = (ServerSocketChannel) selectionKey.channel();
+        channel.configureBlocking(false);
+        SocketChannel socketChannel = channel.accept();
+        if (selectionKey.isAcceptable()) {
+          System.out.println("========isAcceptable========");
+          System.out.println(socketChannel);
+        } else if (selectionKey.isConnectable()) {
+          System.out.println("========isConnectable=========");
+
+        } else if (selectionKey.isReadable()) {
+          System.out.println("========isReadable=========");
+        } else if (selectionKey.isValid()) {
+          System.out.println("========isValid=========");
+        } else if (selectionKey.isWritable()) {
+          System.out.println("========isWritable=========");
+        }
+      }
     }
   }
 }
