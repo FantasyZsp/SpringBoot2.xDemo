1d76b09ec591c89c57d2749f6256876ab36ba038
ZSP-LAPTOP <541054738@qq.com>
2019-12-06 22:45:39 +0800

    save

--- commons/src/main/java/xyz/mydev/jackson/dto/JobQueueDTO.java
+++ commons/src/main/java/xyz/mydev/jackson/dto/JobQueueDTO.java
@@ -2,7 +2,6 @@ package xyz.mydev.jackson.dto;
 
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
