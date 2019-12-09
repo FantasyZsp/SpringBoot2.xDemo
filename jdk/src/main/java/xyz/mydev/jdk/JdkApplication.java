d8b07c1118e9c31ecced6c85d00b245a6e92cc2a
ZSP-LAPTOP <541054738@qq.com>
2019-12-09 23:27:27 +0800

    mark: https bug...

--- jdk/src/main/java/xyz/mydev/jdk/JdkApplication.java
+++ jdk/src/main/java/xyz/mydev/jdk/JdkApplication.java
@@ -1,7 +1,25 @@
 package xyz.mydev.jdk;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+
 /**
  * @author ZSP
  */
 public class JdkApplication {
+  public static void main(String[] args) throws Exception {
+
+    HttpClient httpClient = HttpClient.newBuilder().connectTimeout(Duration.ofMillis(1000))
+      .build();
+    HttpRequest requestBuilder = HttpRequest.newBuilder().uri(URI.create("https://www.baidu.com"))
+      .GET().build();
+    HttpResponse<String> result = httpClient.send(requestBuilder, HttpResponse.BodyHandlers.ofString());
+    System.out.println(result);
+  }
 }
+
+
+
