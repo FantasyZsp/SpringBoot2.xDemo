5e510d24a43692509025145992d579e3f1be5083
ZSP-LAPTOP <541054738@qq.com>
2019-12-10 21:09:59 +0800

    save

--- jdk/src/main/java/xyz/mydev/jdk/Httpclientdemo.java
+++ jdk/src/main/java/xyz/mydev/jdk/Httpclientdemo.java
@@ -1,21 +1,21 @@
 package xyz.mydev.jdk;
 
 import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLParameters;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 import java.security.cert.X509Certificate;
 import java.time.Duration;
-import java.util.Properties;
 
-/**
- * @author ZSP
- */
-public class JdkApplication {
-  public static void main(String[] args) throws Exception {
+public class Httpclientdemo {
+  public static void main(String[] args) throws NoSuchAlgorithmException, Exception, InterruptedException {
     TrustManager[] trustAllCertificates = new TrustManager[]{new X509TrustManager() {
       @Override
       public X509Certificate[] getAcceptedIssuers() {
@@ -33,26 +33,20 @@ public class JdkApplication {
       HostnameVerifier trustAllHostnames = (hostname, session) -> true;
     }};
 
-    System.out.println(System.getenv("JAVA_HOME"));
-//    System.setProperty("javax.net.ssl.trustStore","C:\\Program Files\\Java\\jdk-11.0.2\\lib\\security\\cacerts");
-
+    int timeoutInSeconds = 2;
+    SSLParameters sslParams = new SSLParameters();
+    sslParams.setEndpointIdentificationAlgorithm("");
 //    SSLContext sc = SSLContext.getInstance("SSL");
+    System.setProperty("jdk.internal.httpclient.disableHostnameVerification", "true");
 //    sc.init(null, trustAllCertificates, new SecureRandom());
-//
-    final Properties props = System.getProperties();
-    props.setProperty("jdk.internal.httpclient.disableHostnameVerification", Boolean.TRUE.toString());
-
-//    System.setProperty("javax.net.ssl.trustStore", "C:/Program Files/Java/jdk-11.0.2/lib/security/jssecacerts");
-//    System.setProperty("jdk.tls.server.protocols", "TLSv1.3");
-    HttpClient httpClient = HttpClient.newBuilder().connectTimeout(Duration.ofMillis(500000))
+    HttpClient httpClient = HttpClient.newBuilder().connectTimeout(Duration.ofMillis(timeoutInSeconds * 1000))
 //      .sslContext(sc)
+      .sslParameters(sslParams)
       .build();
-    HttpRequest requestBuilder = HttpRequest.newBuilder().uri(URI.create("http://www.baidu.com/"))
+    HttpRequest requestBuilder = HttpRequest.newBuilder().uri(URI.create("https://www.baidu.com"))
       .GET().build();
-    HttpResponse<String> result = httpClient.send(requestBuilder, HttpResponse.BodyHandlers.ofString());
+    HttpResponse<String> result = httpClient.send(requestBuilder, HttpResponse.BodyHandlers.ofString()); //发送请求
     System.out.println(result);
   }
+
 }
-
-
-
