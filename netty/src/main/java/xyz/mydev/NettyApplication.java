c0aa5ae0fbb32e876557728cfda664113b73380f
zsp-laptop <541054738@qq.com>
2020-01-04 21:22:57 +0800

    save

--- netty/src/main/java/xyz/mydev/NettyApplication.java
+++ netty/src/main/java/xyz/mydev/NettyApplication.java
@@ -1,7 +1,12 @@
 package xyz.mydev;
 
+import io.netty.channel.nio.NioEventLoopGroup;
+
 /**
  * @author ZSP
  */
 public class NettyApplication {
+  public static void main(String[] args) {
+    NioEventLoopGroup bossGroup = new NioEventLoopGroup();
+  }
 }
