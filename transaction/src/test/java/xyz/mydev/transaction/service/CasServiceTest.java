af1d9ba0a54623b08fbe302e3e81e8074905c351
zhaoshipan <541054738@qq.com>
2020-01-10 11:21:31 +0800

    test: jdbc类型映射

--- transaction/src/test/java/xyz/mydev/transaction/service/CasServiceTest.java
+++ transaction/src/test/java/xyz/mydev/transaction/service/CasServiceTest.java
@@ -2,7 +2,18 @@ package xyz.mydev.transaction.service;
 
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import xyz.mydev.transaction.RootTest;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Objects;
 
 /**
  * @author ZSP
@@ -10,6 +21,8 @@ import xyz.mydev.transaction.RootTest;
 public class CasServiceTest extends RootTest {
   @Autowired
   private CasService casService;
+  @Autowired
+  private JdbcTemplate jdbcTemplate;
 
   @Test
   public void compareAndAddAge() {
@@ -24,5 +37,33 @@ public class CasServiceTest extends RootTest {
       e.printStackTrace();
     }
     System.out.println("finish..");
+  }
+
+  @Test
+  public void testJdbcMapping() throws SQLException {
+    Connection conn = Objects.requireNonNull(jdbcTemplate.getDataSource()).getConnection();
+    String sql = "SELECT * FROM  config_discount where 1=2";
+    Statement stmt = conn.createStatement();
+    ResultSet rs = stmt.executeQuery(sql);
+    ResultSetMetaData rsd = rs.getMetaData();
+    int columnCount = rsd.getColumnCount();
+    Map<String, Integer> columnType = new LinkedHashMap<>();
+    for (int i = 1; i <= columnCount; i++) {
+      columnType.put(rsd.getColumnName(i).toLowerCase(), rsd.getColumnType(i));
+    }
+
+    System.out.println(columnType);
+  }
+
+  @Test
+  public void testDate() throws SQLException {
+    Date date = new Date();
+    System.out.println(date.toInstant());
+  }
+
+  @Test
+  public void testTimeStamp() throws SQLException {
+    Date date = new Date();
+    System.out.println(date.toInstant());
   }
 }
