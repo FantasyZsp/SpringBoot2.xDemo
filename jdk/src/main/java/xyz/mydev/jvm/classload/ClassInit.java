f5e9bc86cfcc4f369f8e72d6564d18f950c753e5
zsp-laptop <541054738@qq.com>
2020-04-13 22:02:35 +0800

    classload

---
+++ jdk/src/main/java/xyz/mydev/jvm/classload/ClassInit.java
@@ -0,0 +1,37 @@
+package xyz.mydev.jvm.classload;
+
+/**
+ * 类初始化
+ *
+ * @author ZSP
+ */
+public class ClassInit {
+  public static void main(String[] args) {
+    System.out.println(Child.CHILD_FINAL_ID);
+    System.out.println("Parent and Child has no loaded");
+    System.out.println(Child.FINAL_NUMBER);
+  }
+}
+
+class Parent {
+  /**
+   * 外部仅访问此final变量，不会导致初始化
+   */
+  public static final String ID = "parentId";
+  public static String NAME = "parentName";
+  public static final Integer FINAL_NUMBER = 1;
+  public static Integer NUMBER = 1;
+
+  static {
+    System.out.println("Parent static invoke");
+  }
+}
+
+class Child extends Parent {
+  public static final String CHILD_FINAL_ID = "childId";
+  public static String CHILD_NAME = "childName";
+
+  static {
+    System.out.println("Child static invoke");
+  }
+}
---
+++ jdk/src/main/java/xyz/mydev/jvm/classload/RefInCycle.java
@@ -0,0 +1,64 @@
+package xyz.mydev.jvm.classload;
+
+/**
+ * @author ZSP
+ */
+public class RefInCycle {
+  public static void main(String[] args) {
+    System.out.println(Const.SERVER_ERR);
+  }
+}
+
+
+class Const {
+
+  static {
+    System.out.println("Const static invoke");
+  }
+
+
+  static {
+    System.out.println("Const static invoke after SUCCESS_ENTITY");
+  }
+
+  public static final Integer SUCCESS = 200;
+  public static final Integer BAD_REQUEST = 400;
+  public static final Integer SERVER_ERR = 500;
+
+  public static final ResponseResult SUCCESS_ENTITY = new ResponseResult(Const.SUCCESS, null);
+
+
+}
+
+class ResponseResult {
+
+  private Integer code;
+
+  private String message;
+
+  private Object data;
+
+  public ResponseResult() {
+    System.out.println("ResponseResult constructor1 invoke");
+  }
+
+  static {
+    System.out.println("ResponseResult static invoke");
+  }
+
+  {
+    System.out.println("ResponseResult object invoke");
+  }
+
+  public ResponseResult(Integer code, String message) {
+    System.out.println("ResponseResult constructor2 invoke");
+    this.code = code;
+    this.message = message;
+  }
+
+  public ResponseResult(Integer code, String message, Object data) {
+    this.code = code;
+    this.message = message;
+    this.data = data;
+  }
+}
