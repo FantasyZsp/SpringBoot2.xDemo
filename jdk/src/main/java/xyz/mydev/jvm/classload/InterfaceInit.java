e6890bc8545fbfb9412c6ce9d99f7b624db89085
zsp-laptop <541054738@qq.com>
2020-04-16 00:54:09 +0800

    classload: 父接口初始化demo

---
+++ jdk/src/main/java/xyz/mydev/jvm/classload/InterfaceInit.java
@@ -0,0 +1,33 @@
+package xyz.mydev.jvm.classload;
+
+import java.util.Random;
+
+/**
+ * @author ZSP
+ */
+public class InterfaceInit {
+
+  public static void main(String[] args) {
+    System.out.println(Child.MSG);
+    System.out.println(ChildClass.MSG);
+    System.out.println("=========");
+    System.out.println(Parent.OBJECT);
+  }
+
+  interface Parent {
+    Object OBJECT = new Object() {
+      {
+        System.out.println("parent init");
+      }
+    };
+  }
+
+  interface Child extends Parent {
+    String MSG = "Child init" + new Random().nextInt(3);
+  }
+
+  static class ChildClass implements Parent {
+    public static final String MSG = "ChildClass init" + new Random().nextInt(3);
+  }
+}
+
