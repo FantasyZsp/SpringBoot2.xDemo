04f89061a6f362f7f629546c640ed9de032b0c12
zsp-laptop <541054738@qq.com>
2020-04-22 11:57:25 +0800

    List: 不可变集合。

--- jdk/src/main/java/xyz/mydev/jdk/stream/list/ListOps.java
+++ jdk/src/main/java/xyz/mydev/jdk/stream/list/ListOps.java
@@ -1,5 +1,8 @@
 package xyz.mydev.jdk.stream.list;
 
+import com.google.common.collect.Lists;
+
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -13,5 +16,13 @@ public class ListOps {
 
     list.sort(null);
     Collections.sort(list);
+
+//    List<Integer> integers = List.of(1, 2, 3);
+//    integers.sort(Integer::compareTo);
+
+    ArrayList<Integer> integers1 = Lists.newArrayList(1, 2, 3);
+    integers1.sort(Integer::compareTo);
   }
+
+
 }
