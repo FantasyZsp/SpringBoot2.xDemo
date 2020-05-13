d5caac13a9cf775b2cffeaaa8faedf407aaffd31
zsp-laptop <541054738@qq.com>
2020-05-14 00:17:16 +0800

    thread: base api

---
+++ thread-demo/src/main/java/xyz/mydev/base/ThreadApi.java
@@ -0,0 +1,12 @@
+package xyz.mydev.base;
+
+/**
+ * @author ZSP
+ */
+public class ThreadApi {
+  public static void main(String[] args) {
+    Thread thread = new Thread();
+    System.out.println(thread.isAlive());
+    thread.start();
+    System.out.println(thread.isAlive());  }
+}
