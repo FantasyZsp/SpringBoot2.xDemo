e241ab69df1a17ca0a5f3ab4910d248986f1e326
zhaoshipan <541054738@qq.com>
2020-06-03 18:38:03 +0800

    TODO: redisson 使用。

---
+++ redisson/src/main/java/xyz/mydev/redisson/config/JacksonConfig.java
@@ -0,0 +1,42 @@
+package xyz.mydev.redisson.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+/**
+ * @author ZSP
+ */
+public class JacksonConfig {
+
+  @Bean
+  @ConditionalOnMissingBean
+  public JavaTimeModule javaTimeModule() {
+    return new JavaTimeModule();
+  }
+
+  @Bean
+  @ConditionalOnMissingBean
+  public Jdk8Module jdk8Module() {
+    return new Jdk8Module();
+  }
+
+  @Bean
+  @ConditionalOnMissingBean
+  public ObjectMapper objectMapper(Jackson2ObjectMapperBuilder builder, JavaTimeModule javaTimeModule, Jdk8Module jdk8Module) {
+    SimpleFilterProvider simpleFilterProvider = new SimpleFilterProvider().setFailOnUnknownId(false);
+    ObjectMapper objectMapper = builder
+      .createXmlMapper(false)
+      .modules(javaTimeModule, jdk8Module)
+      .filters(simpleFilterProvider)
+      .build();
+    objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+    return objectMapper;
+  }
+
+}
---
+++ redisson/src/main/java/xyz/mydev/redisson/config/RedissonConfig.java
@@ -0,0 +1,48 @@
+package xyz.mydev.redisson.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.codec.JsonJacksonCodec;
+import org.redisson.config.Config;
+import org.redisson.config.SingleServerConfig;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.DependsOn;
+
+/**
+ * @author ZSP
+ */
+@Slf4j
+@ConditionalOnProperty(prefix = "redisson", name = "address")
+public class RedissonConfig {
+
+  @Bean
+  @ConditionalOnMissingBean
+  public Config redissonSingleServerConfig(ObjectMapper objectMapper) {
+    Config config = new Config();
+    config.setCodec(new JsonJacksonCodec(objectMapper));
+    return config;
+  }
+
+  @Bean
+  @DependsOn("redissonSingleServerConfig")
+  @ConfigurationProperties(prefix = "redisson")
+  @ConditionalOnMissingBean
+  public SingleServerConfig singleServerConfig(Config redissonSingleServerConfig) {
+    return redissonSingleServerConfig.useSingleServer();
+  }
+
+
+  @Bean
+  @DependsOn("singleServerConfig")
+  @ConditionalOnMissingBean
+  public RedissonClient redissonClient(Config redissonSingleServerConfig) {
+    log.info("redissonClient init. address:{}, database:{}", redissonSingleServerConfig.useSingleServer().getAddress(),
+      redissonSingleServerConfig.useSingleServer().getDatabase());
+    return Redisson.create(redissonSingleServerConfig);
+  }
+}
