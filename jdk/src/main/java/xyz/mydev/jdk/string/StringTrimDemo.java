ea25baf3f14c6b000b753c82c09ef423b8f944b3
ZSP <541054738@qq.com>
2020-09-17 17:03:23 +0800

    test: 原生类型NPE

--- jdk/src/main/java/xyz/mydev/jdk/string/StringTrimDemo.java
+++ jdk/src/main/java/xyz/mydev/jdk/string/StringTrimDemo.java
@@ -2,6 +2,8 @@ package xyz.mydev.jdk.string;
 
 import org.apache.commons.lang3.StringUtils;
 
+import java.util.Objects;
+
 /**
  * trim 只是去掉了前后的空格,原来的值不改变。
  *
@@ -13,6 +15,12 @@ public class StringTrimDemo {
     System.out.println(test.trim());
     System.out.println(test);
     System.out.println(StringUtils.trim(test));
+    Integer integer = null;
+//    System.out.println(1 == integer); NPE
+    System.out.println(Objects.equals(integer, 1));
+    System.out.println(Objects.equals(1, integer));
+    System.out.println(Integer.valueOf(1).equals(null));
+
   }
 
 }
