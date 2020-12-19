982fa8bf74c371f3171e8224aa9e7d9201eb734c
zhaoshipan <541054738@qq.com>
2020-12-20 00:56:41 +0800

    elk: logback结合logstash

---
+++ elk-demo/elk-demo-order-api/src/main/java/xyz/mydev/elkdemo/order/OrderApi.java
@@ -0,0 +1,14 @@
+package xyz.mydev.elkdemo.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ZSP
+ */
+@SpringBootApplication
+public class OrderApi {
+  public static void main(String[] args) {
+    SpringApplication.run(OrderApi.class);
+  }
+}
---
+++ elk-demo/elk-demo-product-api/src/main/java/xyz/mydev/elkdemo/product/ProductApi.java
@@ -0,0 +1,14 @@
+package xyz.mydev.elkdemo.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ZSP
+ */
+@SpringBootApplication
+public class ProductApi {
+  public static void main(String[] args) {
+    SpringApplication.run(ProductApi.class);
+  }
+}
