c04402db1ac6a9d0ddd4362d4647a93b0b8af728
ZSP <541054738@qq.com>
2021-04-16 18:49:20 +0800

    test: |

--- jdk/src/main/java/xyz/mydev/jdk/baseapi/map/NumberTest.java
+++ jdk/src/main/java/xyz/mydev/jdk/baseapi/map/NumberTest.java
@@ -8,8 +8,14 @@ import org.junit.Test;
 public class NumberTest {
 
 
+  /**
+   * 二进制按位或
+   */
   @Test
   public void test() {
     System.out.println(0x1);
+    System.out.println(0x1 << 2);
+    System.out.println(1 | 2);
+    System.out.println(1 | 7);
   }
 }
